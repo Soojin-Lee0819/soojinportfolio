e700e2b68b2acdc3c6d4649325b33aba6d14e081
Soojin Lee <sl7362@nyu.edu>
2021-12-21 10:32:51 +0900

    push

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 
 var animation = true;
 var done = false;
-
-
-
-
-
 if (animation && !done){
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+
+
 tl.fromTo(".name1", { rotateY: 0}, { rotateY: 0, duration: 0.5});
 tl.fromTo(".name1", { rotateY: 0}, { rotateY: 360, duration: 1});
 tl.fromTo(".name2", { rotateY: 0}, { rotateY: 360, duration: 1},"-=0.95");
@@ -19,18 +17,36 @@ tl.fromTo(".name6", { rotateY: 0}, { rotateY: 360, duration: 1},"-=0.95");
 tl.fromTo(".name7", { rotateY: 0}, { rotateY: 360, duration: 1},"-=0.95");
 tl.fromTo(".name8", { rotateY: 0}, { rotateY: 360, duration: 1},"-=0.95");
 tl.fromTo(".name9", { rotateY: 0}, { rotateY: 360, duration: 1},"-=0.95");
-tl.fromTo(".cardBack", { opacity: 1 }, { opacity: 0, duration: 0.3});
+tl.fromTo(".cardBack", { opacity:1}, { opacity:0, duration: 0.1});
+tl.fromTo(".cardBack", { x: "0%" }, { x: "-300%", duration: 0.001});
 tl.to(".introduction",{ opacity:1});
 tl.fromTo(".homeleft", {  x: "-120%" }, { x: "0",duration: 0.3},"-=0.3");
-tl.fromTo("#soojinprofile", { opacity:0 }, { opacity:1,duration: 0.3},"-=0.3");
+tl.fromTo("#soojinprofile", { opacity:0 }, { opacity:1,duration: 0.3},"-=0.1");
 tl.fromTo("#text1, #text2", {  opacity:0 }, { opacity:1,duration: 0.3});
 done = true;
 
+tl.fromTo(".span1",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1});
+
+tl.fromTo(".span2",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span3",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span4",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span5",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span6",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span7",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span8",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
+
+tl.fromTo(".span9",{yPercent:-30, ease:Power2.easeInOut}, {yPercent:0, ease:Power2.easeInOut, duration: 1},"-=0.9");
 };
 
 
 function name1() {
-  document.getElementById("text2").innerHTML = "also a playful person specializing in having fun, sharing fun stuff with others."
+  document.getElementById("text2").innerHTML = "also a playful person specializing in having fun, <br> creating something fun, and sharing it with others."
 }
 
 function name2() {
@@ -38,19 +54,22 @@ function name2() {
 }
 
 
-
-
-
 var element = document.querySelector('h1');
+
 
 element.addEventListener('mouseover', onMouseOver);
 
+
+
 function onMouseOver(event) {
     var trg = event.target;
+
     if(trg.tagName.toLowerCase() === 'span') {
+
+
         var tl = new TimelineLite();
         
-        tl.to(trg, 1, {yPercent:-50, ease:Power2.easeInOut});
+        tl.to(trg, 1, {yPercent:-30, ease:Power2.easeInOut});
         tl.to(trg, 1, {yPercent:0, ease:Power2.easeInOut});
     }
 }
